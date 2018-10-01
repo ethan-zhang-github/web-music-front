76a92772cb286dfbe20b0dcfe5672133405c4454
JUST1984 <yixiezi1994@gmail.com>
2018-10-01 15:48:15 +0800

    播放器页面初步开发

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="song-list">
     <ul>
-      <li class="item" v-for="(song, index) in songs" :key="index">
+      <li class="item" v-for="(song, index) in songs" :key="index"
+          @click="selectItem(song, index)">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -22,6 +23,9 @@ export default {
   methods: {
     getDesc (song) {
       return `${song.singer}·${song.album}`
+    },
+    selectItem (item, index) {
+      this.$emit('select', item, index)
     }
   }
 }
